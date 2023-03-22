5394c5938b24e8766175ed79d70511d738603d92
James Seton <jamesseton6@gmail.com>
2023-03-22 16:01:24 +1030

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,31 @@
+import clock from "clock";
+import * as document from "document";
+import { preferences } from "user-settings";
+
+function zeroPad(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+// Update the clock every minute
+clock.granularity = "minutes";
+
+// Get a handle on the <text> element
+const myLabel = document.getElementById("myLabel");
+
+// Update the <text> element every tick with the current time
+clock.ontick = (evt) => {
+  let today = evt.date;
+  let hours = today.getHours();
+  if (preferences.clockDisplay === "12h") {
+    // 12h format
+    hours = hours % 12 || 12;
+  } else {
+    // 24h format
+    hours = zeroPad(hours);
+  }
+  let mins = zeroPad(today.getMinutes());
+  myLabel.text = `${hours}:${mins}`;
+}
