407c2d8bfc7581802cf310b4337bdd47e0ea68e6
James Seton <jamesseton6@gmail.com>
2023-03-22 22:28:32 +1030

    Added Battery

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ import * as document from "document";
 import { preferences } from "user-settings";
 import { me as appbit } from "appbit";
 import { today } from "user-activity";
+import { battery } from "power";
+import { charger } from "power";
 
 function zeroPad(i) {
   if (i < 10) {
@@ -16,11 +18,17 @@ clock.granularity = "minutes";
 
 // Get a handle on the <text> element
 const myLabel = document.getElementById("myLabel");
-const stepsLabel = document.getElementById("stepsLabel")
-const goodWork = document.getElementById("goodWork")
+const stepsLabel = document.getElementById("stepsLabel");
+const goodWork = document.getElementById("goodWork");
+const batteryLabel = document.getElementById("batteryLabel");
+const batteryIcon = document.getElementById("batteryIcon");
 
+batteryLabel.text = ((battery.chargeLevel) + "%")
+batteryCalc();
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
+  
+  //Clock
   let now = evt.date;
   let hours = now.getHours();
   if (preferences.clockDisplay === "12h") {
@@ -33,18 +41,46 @@ clock.ontick = (evt) => {
   let mins = zeroPad(now.getMinutes());
   myLabel.text = `${hours}:${mins}`;
   
+  // Step count
   if (appbit.permissions.granted("access_activity")) {
     let stepsValue = (today.adjusted.steps);
     try{
       if (stepsValue >= 10000){
-        goodWork.style.opacity = 1
+        goodWork.style.opacity = 1;
       } else {
-        goodWork.style.opacity = 0
+        goodWork.style.opacity = 0;
       }
         console.log(stepsValue);
         stepsLabel.text = stepsValue;
     } catch {
         stepsLabel.text = 0;
     }
- }
+  }
+  
+}
+
+battery.onChange = (charger, evt) => {
+   
+  batteryCalc();
+
+}
+
+function batteryCalc(){
+  batteryLabel.text = ((battery.chargeLevel) + "%");
+    
+  if (charger.connected === true) {
+    batteryIcon.href = "batteries/charging-battery.png"
+  } else {
+    if (battery.chargeLevel > 90) {
+      batteryIcon.href = "batteries/full-battery.png";
+    } else if (battery.chargeLevel > 60){
+      batteryIcon.href = "batteries/used-battery.png"
+    } else if (battery.chargeLevel > 40){
+      batteryIcon.href = "batteries/half-battery.png"
+    } else if (battery.chargeLevel > 25){
+      batteryIcon.href = "batteries/low-battery.png"
+    } else if (battery.chargeLevel > 0){
+      batteryIcon.href = "batteries/recharge-battery.png"
+    }
+  }
 }
